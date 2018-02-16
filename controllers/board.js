46d41a3f974ce0191057ab35decfd60f3c0f2205
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-02-15 22:41:32 -0800

    updated admin sql query'

--- controllers/board.js
+++ controllers/board.js
@@ -9,27 +9,25 @@ var dataCleanse = require('./dataCleanse.js')
 //login get and post routes
 router.get("/allBoards", isLoggedIn, function(req, res){
 	var user = String(req.user.id)
-	var username;
-	
 
 	db.sequelize.query('SELECT distinct username from users WHERE id = :userid',
   { replacements: { userid: [user] }, type: sequelize.QueryTypes.SELECT }
   ).then(function(results){
-  	username = results[0].username
+  	var username = results[0].username
   	
   	if (username === 'admin'){
-  		res.render("boards/adminBoard")
+  				db.sequelize.query('SELECT distinct bt.boardtopic, bt.topicdesc, tt.taskdesc, COUNT(DISTINCT CASE WHEN tt.prioritydesc = :p1 THEN tt.userid END) as low, COUNT(DISTINCT CASE WHEN tt.prioritydesc = :p2 THEN tt.userid END) as med, COUNT(DISTINCT CASE WHEN tt.prioritydesc = :p3 THEN tt.userid END) as high FROM topictasks tt JOIN boardtopics bt ON tt.topicid = bt.id JOIN userboards ub ON bt.boardtopic = ub.boardtopic GROUP BY bt.boardtopic,  bt.topicdesc, tt.taskdesc, tt.prioritydesc ORDER BY boardtopic, topicdesc, taskdesc',   { replacements: { p1: 'low', p2:'med', p3:'high' }, type: sequelize.QueryTypes.SELECT }).then(function(results){
+				console.log('results from admin', results)
+				res.render("boards/adminBoard")
+		});
 
   	} else {
 
-	db.sequelize.query('SELECT * from userboards WHERE userid = :userid',
-  { replacements: { userid: [user] }, type: sequelize.QueryTypes.SELECT }
-  ).then(function(results){
-		console.log('results', results)
-		console.log('results at [0]', results[0])
-
-		res.render("boards/allBoards", {results: results});
-
+		db.sequelize.query('SELECT * from userboards WHERE userid = :userid',
+	  { replacements: { userid: [user] }, type: sequelize.QueryTypes.SELECT }
+	  ).then(function(results){
+			console.log('results', results)
+			res.render("boards/allBoards", {results: results});
 	});
  }
 
